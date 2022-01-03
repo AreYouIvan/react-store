9aaeeba46f9c191e1950b88136f7a87bae0044aa
Ivan Garcia <ivangm180@gmail.com>
2022-01-03 11:09:20 -0600

    creating the new files (routes, pages, components and containers)

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,6 +1,12 @@
 import React from "react";
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import "../globalStyles/global.css";
+
+// Containers
+
+// Components
+
+// Pages
 import { Login } from "../containers/Login";
 import { Layout } from "../containers/Layout";
 import { RecoveryPassword } from "../containers/RecoveryPassword";
@@ -14,6 +20,12 @@ const App = () => {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/login" element={<Login />} />
+          <Route exact path="/send-email" element={<SendEmail />} />
+          <Route exact path="/new-password" element={<NewPassword />} />
+          <Route exact path="/account" element={<MyAccount />} />
+          <Route exact path="/signup" element={<CreateAccount />} />
+          <Route exact path="/checkout" element={<Checkout />} />
+          <Route exact path="/orders" element={<Orders />} />
           <Route
             exact path="/recovery-password"
             element={<RecoveryPassword />}
