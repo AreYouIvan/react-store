e62d7a4f59831e86dd04164e6e48d10840a8689b
Ivan Garcia <ivangm180@gmail.com>
2022-01-09 08:58:22 -0600

    working with react use context

--- src/routes/App.js
+++ src/routes/App.js
@@ -2,9 +2,12 @@ import React from "react";
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import "../globalStyles/global.css";
 
-// Containers
+// hooks
+import { useInitialState } from "../hooks/useInitialState"
 
-// Components
+// Context
+
+import { AppContext  } from "../context/AppContext";
 
 // Pages
 import { Login } from "../pages/Login";
@@ -20,26 +23,30 @@ import { Checkout } from "../pages/Checkout";
 import { Orders } from "../pages/Orders";
 
 const App = () => {
+  const initialState = useInitialState()
   return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/login" element={<Login />} />
-          <Route exact path="/send-email" element={<SendEmail />} />
-          <Route exact path="/new-password" element={<NewPassword />} />
-          <Route exact path="/account" element={<MyAccount />} />
-          <Route exact path="/signup" element={<CreateAccount />} />
-          <Route exact path="/checkout" element={<Checkout />} />
-          <Route exact path="/orders" element={<Orders />} />
-          <Route
-            exact path="/recovery-password"
-            element={<RecoveryPassword />}
-          />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+    <AppContext.Provider value={initialState}>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/send-email" element={<SendEmail />} />
+            <Route exact path="/new-password" element={<NewPassword />} />
+            <Route exact path="/account" element={<MyAccount />} />
+            <Route exact path="/signup" element={<CreateAccount />} />
+            <Route exact path="/checkout" element={<Checkout />} />
+            <Route exact path="/orders" element={<Orders />} />
+            <Route
+              exact
+              path="/recovery-password"
+              element={<RecoveryPassword />}
+            />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </AppContext.Provider>
   );
 };
 
