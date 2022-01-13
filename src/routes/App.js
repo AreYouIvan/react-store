f0c0ac18506cc9c94b523449fca97508fcc5e31b
Ivan Garcia <ivangm180@gmail.com>
2022-01-13 10:22:51 -0600

    [FIX] Routing in app

--- src/routes/App.js
+++ src/routes/App.js
@@ -29,17 +29,29 @@ const App = () => {
       <BrowserRouter>
         <Layout>
           <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/login" element={<Login />} />
-            <Route exact path="/send-email" element={<SendEmail />} />
-            <Route exact path="/new-password" element={<NewPassword />} />
-            <Route exact path="/account" element={<MyAccount />} />
-            <Route exact path="/signup" element={<CreateAccount />} />
-            <Route exact path="/checkout" element={<Checkout />} />
-            <Route exact path="/orders" element={<Orders />} />
+            <Route exact path="/react-store/" element={<Home />} />
+            <Route exact path="/react-store/login" element={<Login />} />
             <Route
               exact
-              path="/recovery-password"
+              path="/react-store/send-email"
+              element={<SendEmail />}
+            />
+            <Route
+              exact
+              path="/react-store/new-password"
+              element={<NewPassword />}
+            />
+            <Route exact path="/react-store/account" element={<MyAccount />} />
+            <Route
+              exact
+              path="/react-store/signup"
+              element={<CreateAccount />}
+            />
+            <Route exact path="/react-store/checkout" element={<Checkout />} />
+            <Route exact path="/react-store/orders" element={<Orders />} />
+            <Route
+              exact
+              path="/react-store/recovery-password"
               element={<RecoveryPassword />}
             />
             <Route path="*" element={<NotFound />} />
